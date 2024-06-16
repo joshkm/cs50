7171377684bd05b39ce8750d35cfa664a1bc3747
safari <joshuakathenya@gmail.com>
2024-06-17 00:46:31 +0200

    compare boolean with C

---
+++ compare.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void) {
+
+    int x ;
+    int y ;
+
+    printf("what is x ?");
+    scanf("%d",&x);
+
+    printf("what is y");
+    scanf("%d",&y);
+
+    if (x < y )
+    {
+        printf("x is less than y\n");
+    }
+    return 0;
+}
