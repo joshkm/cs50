366f2e3ffe1e750e19e789206f3fb6bb9e8558e0
safari <joshuakathenya@gmail.com>
2024-06-17 00:51:14 +0200

    boolean with C

--- compare.c
+++ compare.c
@@ -15,9 +15,15 @@ int main (void) {
     {
         printf("x is less than y\n");
     }
-    else
+    else if (x > y)
     {
-        printf("x is not less than y\n");
+        printf("x is greater than  y\n");
+    
     }
+    else if (x == y)
+    {
+       printf("x is equal to y\n");
+    }
+    
     return 0;
 }
