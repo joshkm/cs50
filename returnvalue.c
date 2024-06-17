b3aa69ff43c72776cb273745530657d45b6b1b65
safari <joshuakathenya@gmail.com>
2024-06-18 00:35:21 +0200

    return value calculator in c

---
+++ returnvalue.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main (void)
+{
+    int x; 
+    int y;
+
+    printf("x ");
+    scanf("%d",&x);
+
+    printf("y ");
+    scanf("%d",&y);
+
+    printf("%i\n", x + y);
+
+
+    
+}
