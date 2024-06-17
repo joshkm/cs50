294121a303911511aec1236e8f470d3b29a37de2
safari <joshuakathenya@gmail.com>
2024-06-18 00:17:27 +0200

    functions in C

--- loops002.c
+++ loops002.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void dragon(int n);#include <stdio.h>
 
 void dragon(int n);
 
 int main(void)
-{
+
 {
     dragon(30000);
+    
 
 }
 
@@ -15,27 +15,12 @@ void dragon(int n)
 {
    int  counter = 0;
    while (counter < n) 
-}  counter++
-{
+   
+
+   {
     printf("dragon.\n,");
+
+  }
     
 } 
-}
 
-int main(void)
-{
-{
-    dragon(30000);
-    return 0;
-}
-
-void dragon(int n)
-{
-   int  counter = 0;
-   while (counter < n) 
-}
-{
-    printf("dragon.\n,");
-    counter++;
-} 
-}
